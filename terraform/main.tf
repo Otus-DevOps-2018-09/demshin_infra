64d276e7892098257bb5aaac2618268297d35be5
Aleksandr Demshin <demshin@gmail.com>
2018-11-14 11:14:35 +0500

    move app config to separate file

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,67 +4,6 @@ provider "google" {
   region  = "${var.region}"
 }
 
-resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "${var.zone}"
-  count        = "${var.count}"
-
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
-
-  metadata {
-    ssh-keys = "demshin:${file(var.public_key_path)}"
-  }
-
-  tags = ["reddit-app"]
-
-  network_interface {
-    network = "default"
-
-    access_config = {
-      nat_ip = "${google_compute_address.app_ip.address}"
-    }
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "demshin"
-    agent       = false
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
-
-resource "google_compute_address" "app_ip" {
-  name = "reddit-app-ip"
-}
-
-resource "google_compute_firewall" "firewall_puma" {
-  name        = "allow-puma-default"
-  description = "Allow SSH"
-  network     = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
-}
-
 resource "google_compute_firewall" "firewall_ssh" {
   name    = "default-allow-ssh"
   network = "default"
