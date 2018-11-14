eb588a08e2aafbb1b91f6519058592bba001a38e
Aleksandr Demshin <demshin@gmail.com>
2018-11-14 11:21:30 +0500

    move ssh-firewall config to separate file

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,18 +4,6 @@ provider "google" {
   region  = "${var.region}"
 }
 
-resource "google_compute_firewall" "firewall_ssh" {
-  name    = "default-allow-ssh"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
-
 resource "google_compute_project_metadata" "keys" {
   metadata {
     ssh-keys = <<EOF
