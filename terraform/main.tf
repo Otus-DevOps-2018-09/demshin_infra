2dc53e3673335a6c687fe73c7d48883c9df20e50
Aleksandr Demshin <demshin@gmail.com>
2019-04-16 12:17:26 +0500

    separate file for app

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,71 +4,8 @@ provider "google" {
   region  = "${var.region}"
 }
 
-resource "google_compute_instance" "app" {
-  name         = "reddit-app"
-  machine_type = "g1-small"
-  zone         = "${var.zone}"
-  count        = "${var.count}"
-
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
-
-  metadata {
-    ssh-keys = "demshin:${file(var.public_key_path)}"
-  }
-
-  tags = ["reddit-app"]
-
-  network_interface {
-    network       = "default"
-    access_config = {}
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "demshin"
-    agent       = false
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "file" {
-    source      = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
-}
-
-resource "google_compute_firewall" "firewall_puma" {
-  name    = "allow-puma-default"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["9292"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["reddit-app"]
-}
-
 resource "google_compute_project_metadata" "keys" {
   metadata {
     ssh-keys = "appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
-  }
-}
-
-resource "google_compute_firewall" "firewall_ssh" {
-  name    = "default-allow-ssh"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
   }
 }
