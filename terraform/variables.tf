f38033077ba9c7c72237ec4fa414a0210151b52b
Aleksandr Demshin <demshin@gmail.com>
2019-04-16 15:37:34 +0500

    add var app_disk_image

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,6 +29,11 @@ variable zone {
   default     = "europe-north1-c"
 }
 
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
 variable "db_disk_image" {
   description = "Disk image for reddit db"
   default     = "reddit-db-base"
