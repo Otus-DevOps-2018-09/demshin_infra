77cd347663697a28f32b16982d8e23f5bc894e8e
Aleksandr Demshin <demshin@gmail.com>
2019-04-30 23:06:05 +0500

    add s3 bucket for tf state

--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -7,7 +7,7 @@ provider "google" {
 module "storage-bucket" {
   source  = "SweetOps/storage-bucket/google"
   version = "0.1.1"
-  name    = ["demshin-storage-bucket-test", "demshin-storage-bucket-test2"]
+  name    = ["demshin-otus-tfstate"]
 }
 
 output "storage-bucket_url" {
